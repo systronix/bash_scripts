86b49100a6386bcbc967509d66706862a2f68733
systronix <bboyes@systronix.com>
2016-09-13 20:45:11 -0600

    more folders, and push

--- push.sh
+++ push.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
-echo Try to do various git pulls
+echo Try to do various git pushes
 echo First: repos which are not libraries
 cd ../Arduino/SALT_CodeUpdates/
-git pull origin master
+git push origin master
 cd ../SALT_reptile/
-git pull origin master
+git push origin master
 cd ../NAP-SALT2-TEST
-git pull origin master
+git push origin master
 
 echo Now library repos
 cd ../libraries/SALT_menu/
-git pull origin master
+git push origin master
 cd ../Systronix_MB85RC256V
-git pull origin master
+git push origin master
 cd ../NAP_pod_load_defs
-git pull origin master
+git push origin master
 cd ../SALT_drawer_locks
-git pull origin master
+git push origin master
 cd ../SALT_settings
-git pull origin master
+git push origin master
 cd ../SALT_UI
-git pull origin master
+git push origin master
 cd ../SALT_utilities
-git pull origin master
+git push origin master
 cd ../SALT_heat_control
-git pull origin master
+git push origin master
 cd ../SALT_JX
-git pull origin master
+git push origin master
 cd ../Systronix_PCA9557
-git pull origin master
+git push origin master
 cd ../Systronix_TMP102
-git pull origin master
+git push origin master
 cd ../SdFat
-git pull origin master
+git push origin master
