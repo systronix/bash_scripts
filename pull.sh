39dd97e885045d043df0a8c86eaffd04c4b62bb9
systronix <bboyes@systronix.com>
2017-05-21 17:28:59 -0600

    added new libs, pwd so you know where pull is attempted

--- pull.sh
+++ pull.sh
@@ -11,79 +11,123 @@ echo
 cd ../NAP-SALT2-TEST
 git pull origin master
 echo 
-cd ../../napserver_2017
+cd ../W5500_Test
+git pull origin master
+echo 
+echo Now library repos
+cd ../libraries
+pwd
+cd XPT2046_Touchscreen
 pwd
 git pull origin master
 echo 
-cd ../Arduino/libraries
+cd ../ILI9341_t3/
 pwd
+git pull origin master
 echo 
-echo Now library repos
+cd ../ILI9341_fonts
 pwd
-cd SALT_FETs/
+git pull origin master
+echo 
+cd ../i2c_t3
+pwd
+git pull origin master
+echo 
+cd ../SALT_exceptions
+pwd
+git pull origin master
+echo
+cd ../SALT_FETs/
+pwd
 git pull origin master
 echo 
 cd ../SALT_menu/
+pwd
 git pull origin master
 echo 
 cd ../Systronix_MB85RC256V
+pwd
 git pull origin master
 echo 
+pwd
 cd ../NAP_pod_load_defs
+pwd
 git pull origin master
 echo 
 cd ../SALT_drawer_locks
+pwd
 git pull origin master
 echo 
 cd ../SALT_drawer_locks
+pwd
 git pull origin master
 echo 
 cd ../SALT_settings
+pwd
 git pull origin master
 echo 
 cd ../SALT_UI
+pwd
 git pull origin master
 echo 
 cd ../SALT_utilities
+pwd
 git pull origin master
 echo 
 cd ../SALT_heat_control
+pwd
 git pull origin master
 echo 
 cd ../SALT_JX
+pwd
 git pull origin master
 echo 
 cd ../Systronix_PCA9557
+pwd
 git pull origin master
 echo 
 cd ../Systronix_TMP102
+pwd
 git pull origin master
 echo 
 cd ../Systronix_TMP275
+pwd
 git pull origin master
 echo
 cd ../SdFat
+pwd
 git pull origin master
 echo 
 cd ../SALT_logging
+pwd
 git pull origin master
 echo
 cd ../Systronix_LCM300
-git pull origin master
-echo
-cd ../TeensyID
+pwd
 git pull origin master
 echo
 cd ../Ethernet2
+pwd
 git pull origin master
 echo
+cd ../Ethernet
+pwd
+git pull origin master
+echo 
 cd ../TeensyMAC
+pwd
 git pull origin master
 echo
 cd ../T3Mac
+pwd
+git pull origin master
+echo
+cd ../TeensyID
+pwd
 git pull origin master
 echo
 cd ../EmonLib
+pwd
 git pull origin master
 echo
 echo Back to the script folder
